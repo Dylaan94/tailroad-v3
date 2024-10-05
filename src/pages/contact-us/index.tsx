e6d5b9e389419fc7f61c80334564db0a4debe0e9
Dylan Brain <dylanbrain@googlemail.com>
2024-10-05 16:00:48 +0100

    feat: added contact us page

--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -1,3 +1,21 @@
+import CallToAction from "@/components/CallToAction";
+import OurBrands from "@/components/OurBrands";
+import ServicesHero from "@/components/ServicesHero";
+
 export default function ContactUs() {
-  return <main>Contact Us</main>;
+  return (
+    <main>
+      <ServicesHero contentSelector="servicesHeroContent" />
+      <CallToAction contentSelector="callToActionContent" />
+      <OurBrands contentSelector="ourBrandsContent" />
+    </main>
+  );
+}
+
+export async function getStaticProps(context: any) {
+  return {
+    props: {
+      messages: (await import(`./messages/${context.locale}.json`)).default,
+    },
+  };
 }
